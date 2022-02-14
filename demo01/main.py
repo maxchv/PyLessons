0075354faf217a42fc49a98aea6a7ac3aa57c81f
Max Shaptala <maxshaptala@gmail.com>
2022-02-14 21:32:47 +0200

    Simple http server for index.html file

--- demo01/main.py
+++ demo01/main.py
@@ -1,8 +1,18 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from pathlib import Path
 
 
 class HttpRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        self.print_info()
+        # ответ
+        self.send_response(200)  # код ответа
+        self.send_header("Content-Type", 'text/html')  # заголовок ответа
+        self.end_headers()  # пустая строка
+        with open(Path("html", "index.html"), 'rb') as f:
+            self.wfile.write(f.read())  # тело ответа
+
+    def print_info(self):
         # Обработка запроса
         print("requestline: " + self.requestline)
         print("path: " + self.path)
@@ -11,13 +21,8 @@ class HttpRequestHandler(BaseHTTPRequestHandler):
         print("header User-Agent: " + self.headers.get("User-Agent"))
         print("header Accept: " + self.headers.get("Accept"))
         print("header Accept-Language: " + self.headers.get("Accept-Language"))
-        # ответ
-        self.send_response(200) # код ответа
-        self.send_header("Content-Type", 'text/html') # заголовок ответа
-        self.end_headers() # пустая строка
-        self.wfile.write(b"<h1>Hello World!</h1>") # тело ответа
 
 
-address = ('localhost', 3000) # localhost | 127.0.0.1
+address = ('localhost', 3000)  # localhost | 127.0.0.1
 http = HTTPServer(address, HttpRequestHandler)
 http.serve_forever()  # запуск сервера
